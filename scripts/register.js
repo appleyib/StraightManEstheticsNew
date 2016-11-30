088ef57d68065ea84586f97e86a01bdb04a0ec15
apple <apple@appledeMacBook-Air.local>
2016-11-30 18:17:42 -0500

    finish register

---
+++ scripts/register.js
@@ -0,0 +1,40 @@
+$(document).ready(function(){
+	$("#savebtn").click(function(){
+        var username=$("#usernamefield").val();
+        var dob = $("#dobfield").val();
+        var gender = $("#genderfield").val();
+        var introduction = $("#textfield5").val();
+        var password1=$("#textfield2").val();
+        var password2=$("#textfield3").val();
+
+        if (username=="" | password1=="" | password2 ==""){
+        	alert("username or password cannot be empty");
+        }else if(password1 != password2){
+        	alert("different password");
+        }else{
+        $.ajax({
+	        url:"/newuser",
+	        type:"POST",
+	        dataType:"JSON",
+	    	contentType:"application/json; charset=utf-8",
+	    	data:JSON.stringify({
+				"userName":username,
+				"birthday":dob,
+				"gender":gender,
+				"introduction":introduction,
+				"password":password1
+			}),
+	    	success:function(response){
+	    		console.log(response);
+	    		window.location = "./CustomerIndex.html?username="+username;
+            },
+            error:function(xhr){
+            	alert(xhr.responseText);
+            }
+	    });
+    	}
+
+    });
+
+    
+})
