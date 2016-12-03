6904b5618457ceb57d99ca300d003933abeefc6c
Xiaodi Lu <kristen.lu@mail.utoronto.ca>
2016-12-03 16:44:38 -0500

    cookies

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 
 var express = require('express');
 var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
 
 var users = require('./routes/routes');
 var app = express();
